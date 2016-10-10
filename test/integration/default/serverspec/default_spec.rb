f0d02132c97ea35cb484cb3f002ada4c0921dd6a
Kierran McPherson <kierranm@gmail.com>
2016-10-10 14:08:25 +1300

    Use lets blocks

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,22 +1,23 @@
 require 'json'
 require 'serverspec'
 
-file_path = os['family'] == 'windows' ? 'C:\node.json' : '/opt/node.json'
+describe 'EC2 Tags plugin' do
+  let(:file_path) { os['family'] == 'windows' ? 'C:\node.json' : '/opt/node.json' }
+  let(:node_data) { JSON.parse(File.read(file_path)) }
 
-describe file(file_path) do
-  it { should exist }
-end
-
-node_data = JSON.parse(File.read(file_path))
-
-describe 'created-by' do
-  it 'should be test-kitchen' do
-    expect(node_data['ec2']['tags']['created-by']).to eq 'test-kitchen'
+  describe file(file_path) do
+    it { should exist }
   end
-end
 
-describe 'example-tag' do
-  it 'should be example' do
-    expect(node_data['ec2']['tags']['example-tag']).to eq 'example'
+  describe 'created-by' do
+    it 'should be test-kitchen' do
+      expect(node_data['ec2']['tags']['created-by']).to eq 'test-kitchen'
+    end
+  end
+
+  describe 'example-tag' do
+    it 'should be example' do
+      expect(node_data['ec2']['tags']['example-tag']).to eq 'example'
+    end
   end
 end
