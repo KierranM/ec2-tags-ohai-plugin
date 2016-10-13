a717394d90e6144304a9a0ef118a3566843ceb01
Kierran McPherson <kierranm@gmail.com>
2016-10-13 15:43:17 +1300

    Use the AWS Ruby SDK

--- templates/default/ec2-tags.rb
+++ templates/default/ec2-tags.rb
@@ -5,54 +5,27 @@ Ohai.plugin(:EC2Tags) do
 
   depends 'ec2'
 
-  def awscli_installed?
-    shell_out('which aws').exitstatus.zero? || shell_out('where.exe aws').exitstatus.zero?
-  end
-
-  def aws_ps_tools_installed?
-    shell_out("#{powershell} Get-Command -ErrorAction SilentlyContinue Get-EC2Tag").exitstatus.zero?
-  end
-
-  def powershell
-    'powershell.exe -ExecutionPolicy Unrestricted'
-  end
-
-  def get_cli_tags
-    instance_id = ec2['instance_id']
-
-    so = shell_out("aws ec2 describe-tags --filters Name=resource-id,Values=#{instance_id}")
-    if so.exitstatus.zero?
-      ec2[:tags] = JSON.parse(so.stdout)['Tags'].map {|tag| {tag['Key'] => tag['Value']}}.inject(Mash.new, :merge)
-    else
-      ohai
-    end
-  end
-
-  def get_ps_tags
-    instance_id = ec2['instance_id']
-    so = shell_out(%(#{powershell} "Get-EC2Tag -Filter @{ Name='resource-id'; Values='#{instance_id}'} | ConvertTo-Json"))
-    if so.exitstatus.zero?
-      ec2[:tags] = JSON.parse(so.stdout).map {|tag| {tag['Key'] => tag['Value']}}.inject(Mash.new, :merge)
-    else
-      ohai
-    end
-  end
-
   collect_data do
-    if awscli_installed?
-      get_cli_tags
-    else
-      Ohai::Log.debug('AWS CLI is not installed')
-    end
-  end
+    begin
+      require 'aws-sdk-core'
 
-  collect_data :windows do
-    if awscli_installed?
-      get_cli_tags
-    elsif aws_ps_tools_installed?
-      get_ps_tags
-    else
-      Ohai::Log.debug('Neither AWS CLI, or AWS powershell tools are installed')
+      client = Aws::EC2::Client.new
+      resp = client.describe_tags(
+        filters: [
+          {
+            name: 'resource-id',
+            values: [
+              ec2['instance_id']
+            ]
+          }
+        ]
+      )
+
+      ec2[:tags] = resp.tags.map { |tag| { tag.key => tag.value } }.inject(Mash.new, :merge)
+    rescue Aws::EC2::Errors::ServiceError => e
+      Ohai::Log.warn("Error while retrieving tags from AWS: #{e.message}")
+    rescue LoadError
+      Ohai::Log.warn('AWS SDK Gem is not installed, unable to retrieve tags')
     end
   end
 end
