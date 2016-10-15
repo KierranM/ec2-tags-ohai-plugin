fccba0f7b15269702640b8ddae9712eadeb6a223
Kierran McPherson <kierranm@gmail.com>
2016-10-15 19:24:27 +1300

    0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kierranm@gmail.com'
 license 'apachev2'
 description 'Ohai plugin for getting EC2 Tags'
 long_description 'Ohai plugin for getting EC2 Tags'
-version '0.2.0'
+version '0.2.1'
 issues_url 'https://github.com/KierranM/ec2-tags-ohai-plugin/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/KierranM/ec2-tags-ohai-plugin' if respond_to?(:source_url)
 
