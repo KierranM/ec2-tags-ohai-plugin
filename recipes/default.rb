f48f2f4fe920d5c672ec36037d3c29f1629ebb51
Thomas Path <thomas.path@coremedia.com>
2017-09-10 21:54:08 +0200

    updated cookbook to work with aws-sdk 3 (#6)
    * updated cookbook to work with aws-sdk 3
    
    * Removed aws-sdk version pinning

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,9 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-chef_gem 'aws-sdk' do
+chef_gem 'aws-sdk-ec2' do
   compile_time true
-  version node['aws']['aws_sdk_version'] if node.key? 'aws'
 end
 
 ohai_plugin 'ec2-tags' do
