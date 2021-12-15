8f0385ee083d2266de7caf756e56b745efb9ebb4
Jason G. Helfman <helfman@calpoly.edu>
2021-12-15 09:20:43 -0800

    - revert transport adjustments for kitchen - chase chef_version for ohai deprecation

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license 'Apache-2.0'
 description 'Ohai plugin for getting EC2 Tags'
 version '0.2.5'
 
+chef_version '>=15.3'
+
 issues_url 'https://github.com/KierranM/ec2-tags-ohai-plugin/issues'
 source_url 'https://github.com/KierranM/ec2-tags-ohai-plugin'
 
